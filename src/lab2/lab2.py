f1df169201baf5666030003cf33ffaebdbac0e05
maxim.kozlyuk@yandex.ru <vypzyx-6mAkbi-qyvdit>
2020-03-30 00:34:26 +0400

    lab 2 done

--- src/lab2/lab2.py
+++ src/lab2/lab2.py
@@ -1,5 +1,3 @@
-from math import sqrt
-
 from src.lab2.heat_removing import choose_calculation
 from src.lab2.showers import ShowersTable
 from src.properties import PropertiesFile
@@ -15,12 +13,67 @@ from src.properties import PropertiesFile
 
 
 class HarmfulSubstances(object):
-    """Содержит в себе логику расчета пункта Б для случая Pk < 0.4"""
+    """Базовый класс для логики расчета пункта Б"""
     def __init__(self) -> None:
         super().__init__()
 
-    def Pk(self, C_rz_, C0_):
-        return
+    def resolvePkCase(self, Pk_):
+        try:
+            if Pk_ < 0.4:
+                return LowPkCalculations()
+            else:
+                return HighPkCalculations()
+        except Exception as e:
+            print(e)
+            raise Exception("Ошибка при выборе метода расчета в зависимости от Pk")
+
+    # todo 2 разные формулы в методичке ??? (C_rz_ - PDK_) * (C_rz_ - C0_)
+    def Pk(self, C_rz_, C0_, PDK_):
+        return (C_rz_ - PDK_) / (C_rz_ - C0_)
+
+
+# todo implement
+class LowPkCalculations(HarmfulSubstances):
+    """Расчеты при PK < 0.4"""
+    def __init__(self) -> None:
+        super().__init__()
+
+    def F0_1(self, x_, Pk_, n_):
+        raise Exception("Функционал не реализованн")
+
+    def V0(self, Uv_, m_, F0_, x_):
+        raise Exception("Функционал не реализованн")
+
+    def tox(self, t_rz_, t_norm_, n_, F0_, x_):
+        raise Exception("Функционал не реализованн")
+
+
+class HighPkCalculations(HarmfulSubstances):
+    """Расчеты при PK >= 0.4"""
+    def __init__(self) -> None:
+        super().__init__()
+
+    def F0_1(self, x_, Pk_, n_):
+        return ((x_ + 3.7 * Pk_ - 1.5) / (0.75 * n_)) ** 2
+
+    def V0(self, Uv_, m_, F0_, x_):
+        sub_big = (0.8 * m_ * (F0_ ** (1/2)) - x_)
+        big = (0.55 + 0.14 * sub_big)
+        return Uv_ / big
+
+    def tox(self, t_rz_, t_norm_, n_, F0_, x_):
+        return t_rz_ - ((t_rz_ - t_norm_) / (0.45 + 0.25 * (0.75 * n_ * (F0_ ** (1/2)) - x_)))
+
+
+def resolve_m_val(m_):
+    m_alpha = None
+    m_val = None
+    if type(m_) is not float:
+        m_alpha = float(m_[1])
+        m_val = float(m_[0])
+        return m_val
+    else:
+        return m_
 
 
 props = PropertiesFile("lab2.properties")
@@ -65,7 +118,8 @@ print("2. Выбранный душ:")
 print(shower, "F0 =", shower.square, "м^2")
 
 # 3
-V0 = shower_calculation.V0(Uv, x, shower.square, shower.m())
+m = resolve_m_val(shower.m())
+V0 = shower_calculation.V0(Uv, x, shower.square, m)
 print("3. Скорость выхода воздуха:")
 print("V0 =", V0, "м/с^2")
 
@@ -85,10 +139,28 @@ print("6. Температура воздуха на выходе патрубк
 print("tox =", tox, "°С")
 
 print("\nБ. Удаление вредных веществ")
+harmful_substances = HarmfulSubstances()
 
 # 1
+Pk = harmful_substances.Pk(C_rz, C0, PDK)
+print("1. Отношение концентраций:")
+print("Pk =", Pk)
+harmful_substances = harmful_substances.resolvePkCase(Pk)
 
-print("1. Отношение концентраций")
+# 2
+print("2. Площадь живого сечения:")
+harmful_F0_1 = harmful_substances.F0_1(x, Pk, shower.n)
+print("F0' =", harmful_F0_1, "м^2")
 
-input("Press ENTER to exit program.")
+# 3
+print("3. Скорость выхода воздуха:")
+harmful_V0 = harmful_substances.V0(Uv, resolve_m_val(shower.m()), shower.square, x)
+print("V0 =", harmful_V0, "м/с")
+
+# 4
+print("Температура на выходе патрубка:")
+harmful_tox = harmful_substances.tox(t_rz, t_norm, shower.n, shower.square, x)
+print("tox =", harmful_tox, "°С")
+
+input("\nPress ENTER to exit program.")
 exit(0)
